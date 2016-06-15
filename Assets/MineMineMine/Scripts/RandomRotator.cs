7bd6432a8c13e3f0441d16cf18216980b218824f
m00n <arcybartek@gmail.com>
2016-06-15 16:59:21 +0200

    asteroid graphics

---
+++ Assets/MineMineMine/Scripts/RandomRotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class RandomRotator : MonoBehaviour
+{
+	public float Tumble;
+	private Rigidbody _rigidbody;
+
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+		SetRandomRotation();
+		ApplyRandomSpin();
+	}
+
+	private void ApplyRandomSpin()
+	{
+		_rigidbody.angularVelocity = Random.insideUnitSphere * Tumble;
+	}
+
+	private void SetRandomRotation()
+	{
+		transform.rotation = Random.rotation;
+	}
+}
