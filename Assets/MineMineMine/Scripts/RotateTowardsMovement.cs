0029c4df21edf78e004b3dc61753d14725dd608b
m00n <arcybartek@gmail.com>
2016-06-15 17:20:17 +0200

    yield graphic

---
+++ Assets/MineMineMine/Scripts/RotateTowardsMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RotateTowardsMovement : MonoBehaviour
+{
+
+	private Rigidbody _rigidbody;
+
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+	}
+}
