ec9f3b137c7eb4abef8d5e5b6eb006973367a79b
Bartosz Jędrasik <Bartosz Jdrasik>
2016-09-08 01:08:32 +0200

    Scattershot particle burst amount self-adjusts to hitbox width

--- Assets/MineMineMine/Scripts/Behaviours/ScattershotMissile.cs
+++ Assets/MineMineMine/Scripts/Behaviours/ScattershotMissile.cs
@@ -18,6 +18,7 @@ public class ScattershotMissile : MonoBehaviour
         _expansionProgress = 0.0f;
         _particleCount = 0;
         _delayedDestruction = GetComponent<DelayedDestruction>();
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
         StartCoroutine(ExpiryCoroutine());
     }
 
@@ -51,7 +52,7 @@ public class ScattershotMissile : MonoBehaviour
                     formerPosition.y,
                     formerPosition.z);
 
-                // lifetime appears to be reset back to startLifetime  after calling SetParticles(), so it's lerped to always
+                // lifetime appears to be reset back to startLifetime after calling SetParticles(), so it's lerped to always
                 // be in sync with width expansion
 
                 _particles[i].lifetime = Mathf.Lerp(_particleSystem.startLifetime, 0, _expansionProgress);
@@ -59,14 +60,32 @@ public class ScattershotMissile : MonoBehaviour
         }
         else
         {
+            AdjustBurstAmount();
             GetParticles();
         }
         _particleSystem.SetParticles(_particles, _particleCount);
     }
 
+    private void AdjustBurstAmount()
+    {
+        ParticleSystem.Burst[] bursts = new ParticleSystem.Burst[_particleSystem.emission.burstCount];
+        _particleSystem.emission.GetBursts(bursts); // the parameter for GetBursts() is actually out
+
+        // referenceWidth and desiredBurstAmountForReferenceWidth are arbitrary, require manual adjustment if particle effect is changed
+        const int referenceWidth = 8;
+        const int desiredBurstAmountForReferenceWidth = 30;
+
+        short desiredBurstAmount = (short)ProportionHelper.LinearOutput(
+            referenceWidth,
+            SceneReference.WeaponManager.ScattershotTargetWidth,
+            desiredBurstAmountForReferenceWidth);
+        bursts[0].maxCount = desiredBurstAmount;
+        bursts[0].minCount = desiredBurstAmount;
+        _particleSystem.emission.SetBursts(bursts);
+    }
+
     private void GetParticles()
     {
-        _particleSystem = GetComponentInChildren<ParticleSystem>();
         _particles = new ParticleSystem.Particle[_particleSystem.maxParticles];
         _particleCount = _particleSystem.GetParticles(_particles); // the parameter for GetParticles() is actually out
         _initialParticlePositions = new List<Vector3>();
