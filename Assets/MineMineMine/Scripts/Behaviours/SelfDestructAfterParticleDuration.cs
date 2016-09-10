4e638e3f522147e4751886c174a206a8befc997f
Bartosz Jędrasik <Bartosz Jdrasik>
2016-09-10 07:47:53 +0200

    Replaced nukes with teleport, teleport object selfdestruct, tweaked weapon balance, reduced player size and tweaked maneuverability to compensate for smaller size

---
+++ Assets/MineMineMine/Scripts/Behaviours/SelfDestructAfterParticleDuration.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using ReptilianCabal.MineMineMine;
+
+public class SelfDestructAfterParticleDuration : MonoBehaviour
+{
+
+    private ParticleSystem _particleSystem;
+
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        StartCoroutine(gameObject.DestroyAfterSeconds(_particleSystem.duration));
+    }
+
+
+
+}
--- Assets/MineMineMine/Scripts/Managers/LifeManager.cs
+++ Assets/MineMineMine/Scripts/Managers/LifeManager.cs
@@ -3,68 +3,68 @@ using UnityEngine.UI;
 
 namespace Assets.MineMineMine.Scripts.Managers
 {
-	public class LifeManager : MonoBehaviour
-	{
+    public class LifeManager : MonoBehaviour
+    {
 
-		public int StartingLives = 3;
-		public Text DebugTextField;
+        public int StartingLives = 3;
+        public Text DebugTextField;
 
-		public bool PlayerAlive { get; set; }
+        public bool PlayerAlive { get; set; }
 
-		private int _livesLeft;
+        private int _livesLeft;
 
-		private void Awake()
-		{
-			RegisterWithSceneReference();
+        private void Awake()
+        {
+            RegisterWithSceneReference();
 
-		}
+        }
 
-		private void Start()
-		{
-			PlayerAlive = true;
-			_livesLeft = StartingLives;
-			UpdateDebugTextField();
-		}
+        private void Start()
+        {
+            PlayerAlive = true;
+            _livesLeft = StartingLives;
+            UpdateDebugTextField();
+        }
 
-		private void RegisterWithSceneReference()
-		{
-			SceneReference.LifeManager = this;
-		}
+        private void RegisterWithSceneReference()
+        {
+            SceneReference.LifeManager = this;
+        }
 
-		private void UpdateDebugTextField()
-		{
-			DebugTextField.text = "Emergency teleports left: " + _livesLeft;
-		}
+        private void UpdateDebugTextField()
+        {
+            DebugTextField.text = "Emergency teleports left: " + _livesLeft;
+        }
 
-		public bool NoLivesLeft()
-		{
-			return _livesLeft <= 0;
-		}
+        public bool NoLivesLeft()
+        {
+            return _livesLeft <= 0;
+        }
 
-		public void Die()
-		{
-			if (NoLivesLeft())
-			{
-				SpawnNuke();
-				PlayerAlive = false;
-			}
-			else
-			{
-				SceneReference.RespawnManager.ShowReticle();
-			}
-		}
+        public void Die()
+        {
+            SpawnTeleportBlink();
+            if (NoLivesLeft())
+            {
+                PlayerAlive = false;
+            }
+            else
+            {
+                SceneReference.RespawnManager.ShowReticle();
+            }
+        }
 
-		private void SpawnNuke()
-		{
-			var nukeSpawnPoint = SceneReference.PlayerSpawnManager.GetCentralPlayer();
-			Instantiate(PrefabReference.Nuke, nukeSpawnPoint.transform.position, PrefabReference.Nuke.transform.rotation);
-		}
+        private void SpawnTeleportBlink()
+        {
+            var blinkSpawnPoint = SceneReference.PlayerSpawnManager.GetCentralPlayer();
+            Instantiate(PrefabReference.TeleportBlink, blinkSpawnPoint.transform.position, PrefabReference.TeleportBlink.transform.rotation);
+        }
 
 
-		public void DecreaseLifeCount()
-		{
-			--_livesLeft;
-			UpdateDebugTextField();
-		}
-	}
+        public void DecreaseLifeCount()
+        {
+            --_livesLeft;
+            UpdateDebugTextField();
+        }
+    }
 }
