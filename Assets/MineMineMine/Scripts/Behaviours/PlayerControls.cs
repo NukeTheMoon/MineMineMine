80f530c6c2a26afce5733a32790e52342d74d6ec
Bartosz Jędrasik <Bartosz Jdrasik>
2016-09-10 09:14:53 +0200

    Added shield graphics

--- Assets/MineMineMine/Scripts/Behaviours/PlayerControls.cs
+++ Assets/MineMineMine/Scripts/Behaviours/PlayerControls.cs
@@ -6,6 +6,8 @@ public class PlayerControls : MonoBehaviour
 
     public float Speed = 10.0f;
     public float AngularSpeed = 2.0f;
+    public float Boost = 1.0f;
+    public bool RecentlyPressedThrust = false;
     private Rigidbody _rigidbody;
 
 
@@ -16,7 +18,7 @@ public class PlayerControls : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(transform.forward * Input.GetAxis("Vertical") * Speed);
+        _rigidbody.AddForce(transform.forward * Input.GetAxis("Vertical") * Speed * Boost);
         _rigidbody.AddTorque(transform.up * Input.GetAxis("Horizontal") * AngularSpeed);
     }
 }
